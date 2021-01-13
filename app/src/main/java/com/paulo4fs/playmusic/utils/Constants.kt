aade62389256c35ddb3deb505d3f40158973f80e
paulo4fs <engpaulofrota@gmail.com>
2021-01-13 13:32:59 -0300

    musicService finalizado

--- app/src/main/java/com/paulo4fs/playmusic/utils/Constants.kt
+++ app/src/main/java/com/paulo4fs/playmusic/utils/Constants.kt
@@ -2,4 +2,9 @@ package com.paulo4fs.playmusic.utils
 
 object Constants {
     const val SONG_COLLECTION = "songs"
+
+    const val MEDIA_ROOT_ID = "root_id"
+
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
 }
